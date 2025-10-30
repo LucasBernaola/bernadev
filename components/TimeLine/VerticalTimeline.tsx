50bbb1fac356a432b3f5561863b271ab72ef30e1
LucasBernaola <bernadev7@gmail.com>
2025-10-30 10:25:40 -0300

    cambios

--- components/TimeLine/VerticalTimeline.tsx
+++ components/TimeLine/VerticalTimeline.tsx
@@ -7,9 +7,14 @@ interface Experience {
 
 const experiences: Experience[] = [
   {
-    name: "Web Developer Trainee / E-learning March - Sept 2023",
+    name: "Freelance Web Development / 2025",
     description:
-      "I worked on redesigning the landing project in WordPress before migrating to a full development in Django and React. I learned to use Django with guidance from the team's Tech Leader, developing skills in both backend and frontend. Additionally, I became familiar with the Scrum methodology and Kanban board, enhancing my communication and problem-solving abilities.",
+      "I work independently developing custom web applications for companies and entrepreneurs, combining functional design with solid technical solutions.",
+  },
+  {
+    name: "Ademass Developer Training / April - May 2024",
+    description:
+      "Simulation of work with the Ademass Developer Training team, a platform that provides tutoring support to students. In this case, we formed a workgroup where we developed a web application. The goal of this practical training was to create a web app using the technologies known to the group while also developing soft skills such as team communication and group management.",
   },
   {
     name: "NO COUNTRY - Work Simulation Practices/ March - April 2024",
@@ -17,9 +22,9 @@ const experiences: Experience[] = [
       "Simulation of Team Work with the No Country group, where we developed an e-commerce platform. The objective of this practical training is to enhance learning and practice of soft skills, work methodologies, team communication, and group management.",
   },
   {
-    name: "Ademass Developer Training / April - May 2024",
+    name: "Web Developer Trainee / E-learning March - Sept 2023",
     description:
-      "Simulation of work with the Ademass Developer Training team, a platform that provides tutoring support to students. In this case, we formed a workgroup where we developed a web application. The goal of this practical training was to create a web app using the technologies known to the group while also developing soft skills such as team communication and group management.",
+      "I worked on redesigning the landing project in WordPress before migrating to a full development in Django and React. I learned to use Django with guidance from the team's Tech Leader, developing skills in both backend and frontend. Additionally, I became familiar with the Scrum methodology and Kanban board, enhancing my communication and problem-solving abilities.",
   },
 ];
 
